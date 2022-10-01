5197118d63ae6d4296f920f16ca411bec01bda46
ashish-21 <ashishchandwani21@gmail.com>
2022-10-01 17:48:43 +0530

    Integrated React Query and used useQuery hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,36 @@ import { Route, Routes, Link } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Superheroes from "./Pages/Superheroes";
 import RQSuperheroes from "./Pages/RQSuperheroes";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const reactQueryClient = new QueryClient();
 
 function App() {
 	return (
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/superheroes">Traditional Super Heroes</Link>
-					</li>
-					<li>
-						<Link to="/rq-superheroes">RQ Super Heroes</Link>
-					</li>
-				</ul>
-			</nav>
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/superheroes" element={<Superheroes />} />
-				<Route path="/rq-superheroes" element={<RQSuperheroes />} />
-			</Routes>
-		</div>
+		<QueryClientProvider client={reactQueryClient}>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/superheroes">
+								Traditional Super Heroes
+							</Link>
+						</li>
+						<li>
+							<Link to="/rq-superheroes">RQ Super Heroes</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/superheroes" element={<Superheroes />} />
+					<Route path="/rq-superheroes" element={<RQSuperheroes />} />
+				</Routes>
+			</div>
+		</QueryClientProvider>
 	);
 }
 
--- src/Pages/RQSuperheroes.tsx
+++ src/Pages/RQSuperheroes.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function RQSuperheroes() {
-	return <div>RQSuperheroes</div>;
+	const QUERY_KEY = "superheroes";
+	const { isLoading, data } = useQuery<any>([QUERY_KEY], () => {
+		return axios.get("http://localhost:4000/superheroes");
+	});
+	console.log(data);
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+	return (
+		<div>
+			<h2>Super Heroes Page - React Query</h2>
+			<div>
+				{data && data?.data
+					? data!.data?.map((e: any) => <p>{e?.name}</p>)
+					: null}
+			</div>
+		</div>
+	);
 }
 
 export default RQSuperheroes;
