3b461c4b6508955d820cfdb7549d5cad6a2b8c33
ashish-21 <ashishchandwani21@gmail.com>
2022-10-01 17:04:57 +0530

    axios and fetching data with axios

--- src/Pages/Superheroes.tsx
+++ src/Pages/Superheroes.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Superheroes() {
-	return <div>Superheroes</div>;
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState<any>([]);
+
+	useEffect(() => {
+		axios.get("http://localhost:4000/superheroes").then((res) => {
+			setData(res.data);
+			setIsLoading(false);
+		});
+	}, []);
+
+	if (isLoading) {
+		return <h2>Loading...</h2>;
+	}
+
+	return (
+		<>
+			<h2>Super Heroes Page</h2>
+			{data &&
+				data.map((hero: any) => {
+					return <div key={hero?.id}>{hero?.name}</div>;
+				})}
+		</>
+	);
 }
 
 export default Superheroes;
