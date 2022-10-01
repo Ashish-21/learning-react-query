9bce1e07f7d38f61a7d6106d94f299f48f0312b9
ashish-21 <ashishchandwani21@gmail.com>
2022-10-01 16:59:39 +0530

    Project Setup with required libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Superheroes from "./Pages/Superheroes";
+import RQSuperheroes from "./Pages/RQSuperheroes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/superheroes">Traditional Super Heroes</Link>
+					</li>
+					<li>
+						<Link to="/rq-superheroes">RQ Super Heroes</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/superheroes" element={<Superheroes />} />
+				<Route path="/rq-superheroes" element={<RQSuperheroes />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Pages/RQSuperheroes.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RQSuperheroes() {
+	return <div>RQSuperheroes</div>;
+}
+
+export default RQSuperheroes;
