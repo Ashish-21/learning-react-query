6cb9307a6d6211bcc09f3309626acfe80944d05e
ashish-21 <ashishchandwani21@gmail.com>
2022-10-09 20:53:51 +0530

    initial data with query

--- src/Pages/RQSuperHeroDetails.tsx
+++ src/Pages/RQSuperHeroDetails.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
@@ -6,18 +6,38 @@ import { useParams } from "react-router-dom";
 const fetchSuperhero = (heroId: any) => {
 	return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
+const QUERY_KEY = "superheroes";
 
 function RQSuperHeroDetails() {
 	const { heroID } = useParams();
-	console.log(heroID);
+	const queryClient = useQueryClient();
 	const { data, isLoading, isError } = useQuery<any>(
 		["superhero", heroID],
-		() => fetchSuperhero(heroID)
+		() => fetchSuperhero(heroID),
+		{
+			initialData: () => {
+				const queryData: any = queryClient.getQueryData([QUERY_KEY]);
+				console.log(queryData?.data);
+				const hero =
+					queryData && queryData?.data
+						? queryData.data?.find(
+								(hero: any) => hero?.id == heroID
+						  )
+						: undefined;
+				if (hero) {
+					console.log("hasd");
+					return {
+						data: hero,
+					};
+				} else {
+					return undefined;
+				}
+			},
+		}
 	);
 	if (isLoading) {
 		return <p>Loading...</p>;
 	}
-	console.log(data);
 	if (isError) {
 		return <p>Error</p>;
 	}
@@ -25,6 +45,7 @@ function RQSuperHeroDetails() {
 		<div>
 			SuperHeroDetails
 			{data && data?.data ? <p>{data?.data?.name}</p> : null}
+			{data && data?.data ? <p>{data?.data?.alterEgo}</p> : null}
 		</div>
 	);
 }
